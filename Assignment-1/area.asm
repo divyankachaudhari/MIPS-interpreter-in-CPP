02167496eb5e999f0ad531c44703164c1a30c7de
Codnay <62608401+Codnay@users.noreply.github.com>
2021-02-22 01:45:46 +0530

    Update area.asm

--- Assignment-1/area.asm
+++ Assignment-1/area.asm
@@ -4,7 +4,7 @@
 				.asciiz 	"The no. of points: "
 
 	Input: 		.asciiz 	"Enter the coordinates of points as space separated integer values: "
-	temp: 		.double 	0.0
+	areaf: 		.double 	0.0
 	area: 		.integer 	0
 	total:		.integer 	0
 
@@ -13,45 +13,59 @@
 .ent main
 main:
 	
-	la 		$a0, 		Intro 	#printing the initial string
-	li		$v0, 		4
+	la 			$a0, 		Intro 	#printing the initial string
+	li			$v0, 		4
 	syscall
 
-	li 		$v0, 		5 		#storing the total number of points	in s0
+	li 			$v0, 		5 		#storing the total number of points	in s0
 	syscall
-	sw 		$v0, 		total
-	move 	$s0, 		$v0
+	sw 			$v0, 		total
+	move 		$s0, 		$v0
 
-	li 		$s1, 		1		#counter increment
-	lw 		$s2, 		area
-	l.d 	f0, 		temp 	#total area in double
+	li 			$s1, 		1		#counter increment
+	lw 			$s2, 		area
+	l.d 		$f0, 		areaf 	#total area in double
 
-	li 		$v0, 		5 		#storing the first point x coordinate
+	li 			$v0, 		5 		#storing the first point x coordinate
 	syscall
-	move 	$s3, 		$v0 
+	move 		$s3, 		$v0 
 
 #To do: Add a case for the number of points 1, throw 0
 
-	li 		$v0, 		5		#storing the first point y coordinate
+	li 			$v0, 		5		#storing the first point y coordinate
 	syscall
-	move 	$s4, 		$v0
+	move 		$s4, 		$v0
 
 
 #To do: Account for negative numbers
 
 	calculation:
 
-		li 			$v0, 		5
+		li 			$v0, 		5						#storing the x coordinate of the next point in the loop
 		syscall		
-		move 		$t0, 		$v0
+		move 		$t0, 		$v0					
 
-		li 			$v0, 		5
+		li 			$v0, 		5						#storing the y coordinate of the next point in the loop
 		syscall
-		move 		$t1, 		$v0
+		move 		$t1, 		$v0					
 
-		subu 		$t2, 		$t1, 		$s3		#storing the value of x2-x1 in an integer register
-		mtc1.d 		$t2, 		$f2					#storing the value of the sum in a floating point register
-		
+		subu 		$t2, 		$t0, 		$s3			#storing the value of x2-x1 in an integer register
+		mtc1.d 		$t2, 		$f2						#storing the value of the x2-x1 in a floating point register
+		mul.d 		$f2, 		$f2, 		0.5 		#getting the value of (x2-x1)/2 in that floating point register
+
+		sum			$t3, 		$t1, 		$s4 		#storing y1+y2
+		mtc1.d 		$t3, 		$f4						#storing y1+y2 in floating point register
+		mul.d 		$f2, 		$f2, 		$f4 		#storing the final value
+
+		li 			$s1, 		$s1,		1			#increasing the counter
+		add.d 		$f0, 		$f0, 		$f2 		#adding this value to the total sum
+		blt 		$s1, 		$s0, 		calculation #looping back into the code
+
+	.end calculation
+
+	li 			$v0, 		3		#printing the final double value
+	l.d 		$f12,		$f0 
+	syscall
 
 
 
